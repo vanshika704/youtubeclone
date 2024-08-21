38d2447a370eb28dfda1504b6bedfbd938e489fb
vanshika704 <sharmavanshi704@gmail.com>
2024-08-21 22:49:15 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import MainPage from './ui/page1';
 
-
-
-
 function App() {
  
   return (
-  <> <Router>
-  <Routes>
-    <Route path="/" element={<MainPage/>} />
- 
-  </Routes>
-</Router></>
+  <>
+     <Router>
+     <Routes>
+     <Route path="/" element={<MainPage/>} />
+     </Routes>
+     </Router></>
       
   )
 }
