2a7bd268154639da5925641d22c6993e94a5a112
vanshika704 <sharmavanshi704@gmail.com>
2024-08-21 23:24:03 +0530

    pixel

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -6,32 +6,39 @@ import { MdSubscriptions } from "react-icons/md";
 import { SiYoutubemusic } from "react-icons/si";
 import { BiSolidVideos } from "react-icons/bi";
 import { HiDownload } from "react-icons/hi";
+
 function SideBar() {
     return (
         <div className="flex bg-black text-gray-900">
             <aside className="flex h-screen w-20 flex-col items-center border-r border-black bg-black">
-                <div className="flex h-[4.5rem] w-full items-center justify-center ">
-                <IoMenu size="24px" color="white" />
+                <div className="flex flex-col items-center h-[4.5rem] w-full justify-center">
+                    <IoMenu size="20px" color="white" />
+                    <span className="text-white mt-1 text-xs">Menu</span>
                 </div>
-                <div className="flex h-[4.5rem] w-full items-center justify-center">
-                <GoHome size="24px" color="white"/>
+                <div className="flex flex-col items-center h-[4.5rem] w-full justify-center">
+                    <GoHome size="20px" color="white"/>
+                    <span className="text-white mt-1 text-xs">Home</span>
                 </div>
-                <div className="flex h-[4.5rem] w-full items-center justify-center ">
-                <SiYoutubeshorts size="24px" color="white"/>
+                <div className="flex flex-col items-center h-[4.5rem] w-full justify-center">
+                    <SiYoutubeshorts size="20px" color="white"/>
+                    <span className="text-white mt-1 text-xs">Shorts</span>
                 </div>
-                <div className="flex h-[4.5rem] w-full items-center justify-center ">
-                <MdSubscriptions  size="24px" color="white" />
+                <div className="flex flex-col items-center h-[4.5rem] w-full justify-center">
+                    <MdSubscriptions size="20px" color="white" />
+                    <span className="text-white mt-1 text-xs">Subscriptions</span>
                 </div>
-                <div className="flex h-[4.5rem] w-full items-center justify-center  ">
-                <SiYoutubemusic  size="24px" color="white" />
+                <div className="flex flex-col items-center h-[4.5rem] w-full justify-center">
+                    <SiYoutubemusic size="20px" color="white" />
+                    <span className="text-white mt-1 text-xs">Music</span>
                 </div>
-                <div className="flex h-[4.5rem] w-full items-center justify-center  ">
-                <BiSolidVideos  size="24px" color="white" />
+                <div className="flex flex-col items-center h-[4.5rem] w-full justify-center">
+                    <BiSolidVideos size="20px" color="white" />
+                    <span className="text-white mt-1 text-xs">You</span>
                 </div>
-                <div className="flex h-[4.5rem] w-full items-center justify-center  ">
-                <HiDownload size="24px" color="white" />
+                <div className="flex flex-col items-center h-[4.5rem] w-full justify-center">
+                    <HiDownload size="20px" color="white" />
+                    <span className="text-white mt-1 text-xs">Downloads</span>
                 </div>
-
             </aside>
         </div>
     );
