6ebaf2d5edacb5c73d0da3f2f69bc27710a3cdc6
vanshika704 <sharmavanshi704@gmail.com>
2024-08-25 19:27:57 +0530

    jjdfnkdn

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import { IoMenu, IoSearch } from "react-icons/io5";
 import { RiVideoAddLine } from "react-icons/ri";
+
 function Navbar() {
   return (
     <div className="bg-black text-white py-3 flex items-center px-4">
@@ -8,18 +9,14 @@ function Navbar() {
         <IoMenu size="24px" color="white" />
       </div>
 
- 
       <img
         src="src/assets/new-youtube-premium-features-1280-6d58ec9cf9__1_-removebg-preview.png"
         alt="YouTube Logo"
         className="h-16 w-auto mx-4 ml-2 object-contain" 
       />
 
-   
       <div className="flex items-center flex-grow ml-auto max-w-2xl">
         <div className="flex items-center w-full bg-black border border-youtubeGray h-10 rounded-l-full overflow-hidden">
-          
-         
           <div className="relative flex items-center w-full">
             <input
               type="text"
@@ -29,11 +26,9 @@ function Navbar() {
           </div>
         </div>
 
-      
         <button
           type="button"
           className="flex items-center justify-center w-16 h-10 bg-youtubeGray rounded-r-full hover:bg-gray-600 mr-4 sm:mr-8 md:mr-16 lg:mr-56"
-
         >
           <IoSearch className="w-6 h-6 text-white" />
         </button>
@@ -62,29 +57,22 @@ function Navbar() {
         </button>
 
         <button
-  type="button"
-  className="p-2.5 bg-transparent text-gray-400 rounded-full"
-> <svg
-            className="h-8 w-8 mt-3 text-white stroke-2"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-<RiVideoAddLine /></svg>
-</button>
+          type="button"
+          className="p-2.5 bg-transparent text-gray-400 rounded-full flex items-center justify-center"
+        >
+          <RiVideoAddLine className="h-8 w-8 text-white" />
+        </button>
 
-<button
-  type="button"
-  className="p-2.5 bg-transparent text-gray-400 rounded-full"
-> <img
-  alt="tania andrew"
-  src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
-  className="relative inline-block h-12 w-12 cursor-pointer rounded-full object-cover object-center"
-  data-popover-target="profile-menu"
-/>
-</button>
+        <button
+          type="button"
+          className="p-2.5 bg-transparent text-gray-400 rounded-full flex items-center justify-center"
+        >
+          <img
+            alt="User Avatar"
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
+            className="h-12 w-12 rounded-full object-cover"
+          />
+        </button>
       </div>
     </div>
   );
