f3082e6250a0e864c8593abeea65898798851d52
vanshika704 <sharmavanshi704@gmail.com>
2024-08-28 20:22:12 +0530

    tanstack installation

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import {
+  QueryClient,
+  QueryClientProvider,
+ 
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <QueryClientProvider client={queryClient}>
     <ChakraProvider>
     <App />
     </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
