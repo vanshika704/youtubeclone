402dbaa2ba7ebf1d5f383f83762f1fdd356c7d69
vanshika704 <sharmavanshi704@gmail.com>
2024-08-31 20:08:28 +0530

    .

--- src/services/fetchvideos.js
+++ src/services/fetchvideos.js
@@ -2,9 +2,8 @@
 import axios from "axios";
 
 const API_URL = "https://pixabay.com/api/videos/";
-const API_KEY = "45732391-f756039ab92580501d5bae5f9"; // Replace with your actual Pixabay API key
+const API_KEY = "45732391-f756039ab92580501d5bae5f9"; 
 
-// Function to search for videos using Pixabay API
 export const searchVideos = async (query) => {
   try {
     const response = await axios.get(API_URL, {
