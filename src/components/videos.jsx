0e25cfa31097f79722701df184bb96fd432d7478
vanshika704 <sharmavanshi704@gmail.com>
2024-09-02 23:19:13 +0530

    .

--- src/components/videos.jsx
+++ src/components/videos.jsx
@@ -253,7 +253,7 @@ function Videos() {
           </div>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
           {videos.length > 0 ? (
             videos.map((video) => (
               <div key={video.id} className="relative group">
