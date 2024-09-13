3e85402f4c4009d0b12688d938ec31adcbf0208e
vanshika704 <sharmavanshi704@gmail.com>
2024-09-13 19:38:08 +0530

    .

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -34,9 +34,9 @@ function Navbar() {
 
         <button
           type="button"
-          className="flex items-center justify-center w-16 h-8 bg-youtubeGray1 border-gray-200 rounded-r-full hover:bg-gray-600 mr-4 sm:mr-8 md:mr-16 lg:mr-56"
+          className="flex items-center justify-center w-16 h-9 bg-youtubeGray1 border-gray-200 rounded-r-full hover:bg-gray-600 mr-4 sm:mr-8 md:mr-16 lg:mr-56"
         >
-          <IoSearch className="w-6 h-6 text-white" />
+          <IoSearch className="w-6 h-6 text-white border-gray-200" />
         </button>
       </div>
 
